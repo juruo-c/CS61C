8119e56353f64e0def146d7bf7730471d4f06c2e
juruo-c <2915601267@qq.com>
2023-07-15 13:25:14 +0800

    23.07.15 lab01 Passed

--- lab01/ex10_ll_cycle.c
+++ lab01/ex10_ll_cycle.c
@@ -3,4 +3,34 @@
 
 int ll_has_cycle(node *head) {
     /* TODO: Implement ll_has_cycle */
+    node* fast_ptr = head;
+    node* slow_ptr = head;
+    while (1)
+    {
+        if (fast_ptr == NULL)
+        {
+            return 0;
+        }
+        if (fast_ptr->next != NULL)
+        {
+            fast_ptr = fast_ptr->next;
+            if (fast_ptr->next != NULL)
+            {
+                fast_ptr = fast_ptr->next;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        else
+        {
+            return 0;
+        }
+        slow_ptr = slow_ptr->next;
+        if (slow_ptr == fast_ptr)
+        {
+            return 1;
+        }
+    }
 }
